9eda193ef80a89c9026ec4f5844d97df2db41528
Zwazel <zwazel@hotmail.com>
2022-07-12 13:10:32 +0200

    sending messages and getting answers!

--- js/chatbot.js
+++ js/chatbot.js
@@ -15,9 +15,36 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         }).then(data => {
         console.log(data);
+        chatbot = data;
+
         title.innerHTML = data.chatbotName;
         madeBy.innerHTML = data.user.username;
     }).catch(error => {
         alert(error.message);
     });
+
+    document.getElementById("chatInputSend").addEventListener("click", sendMessage);
+    document.getElementById("chatInputText").addEventListener("keypress", async (event) => {
+        if (event.key === "Enter") {
+            await sendMessage();
+        }
+    });
 });
+
+async function sendMessage() {
+    const chatInput = document.getElementById("chatInputText");
+    const chatInputValue = chatInput.value;
+
+    if (chatInputValue.length > 0) {
+        const response = await fetch("".concat(baseUrl, "/chatbot/", chatbot.id, "/", chatInputValue), {
+            method: "POST",
+        });
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            chatInput.value = "";
+        } else {
+            throw new Error("Failed to send message");
+        }
+    }
+}
